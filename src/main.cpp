52628c99ad1eba79740ad9de81594d451b1a45b5
LOSEARDES77 <84414230+LOSEARDES77@users.noreply.github.com>
2022-10-14 19:22:23 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <chrono>
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace std;
+int calc(int1024_t num){
+    while(num > 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else {
+            num = num * 3;
+            num = num + 1;
+        };
+        cout << num << endl;
+    };
+    return 0;
+};
+int main() {
+    using chrono::high_resolution_clock;
+    using chrono::duration_cast;
+    using chrono::duration;
+    using chrono::milliseconds;
+    int1024_t a;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Starting at: " << a;
+    auto t1 = high_resolution_clock::now();
+    calc(a);
+    auto t2 = high_resolution_clock::now();
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    cout<< "Process finished in " << ms_int.count() << "ms\n";
+    return 0;
+}
